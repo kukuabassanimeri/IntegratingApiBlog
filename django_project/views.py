1b65051938b421aa10d99c522f5facceba22a3bb
Abassy Kuku Animeri <125487152+kukuabassanimeri@users.noreply.github.com>
2024-04-24 13:08:53 +0000

    styled the page using css

--- django_project/views.py
+++ django_project/views.py
@@ -1,4 +1,13 @@
+from django.http import response
 from django.shortcuts import render
+import requests
 
 def index(request):
-  return render(request, 'template/index.html')
+  response1 = requests.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&')
+  data = response1.json()
+  Weathermessage = data['message']
+
+  # response2 = requests.get('https://random.responsiveimages.io/')
+  # data2 = response2.json()
+  # Image1 = data2['image']
+  return render(request, 'template/index.html', {'Weathermessage': Weathermessage})
